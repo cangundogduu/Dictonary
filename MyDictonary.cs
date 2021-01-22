152af156f3e263fd95262ebc990121ff21d4dbaf
Can Gündoğdu <gundogdu.can@hotmail.com>
2021-01-23 00:47:50 +0300

    Initial commit

---
+++ MyDictonary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class MyDictonary<T1,T2>
+    {
+        T1[] _keys;
+        T2[] _values;
+        public MyDictonary()
+        {
+            _keys = new T1[0];
+            _values = new T2[0];
+        }
+        public void Add (T1 key,T2 value)
+        {
+            T1[] tempKeys = _keys;
+            T2[] tempValues = _values;
+
+            _keys = new T1[tempKeys.Length + 1];
+            _values = new T2[_values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+                _values[i] = tempValues[i];
+            }
+
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+        }
+        public T1[] Keys
+        {
+            get { return _keys; }
+        }
+
+        public T2[] Values
+        {
+            get { return _values; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<int, string> clubs = new MyDictonary<int, string>();
+
+            clubs.Add(01, "Barcelona");
+            clubs.Add(02, "Real Madrid");
+            clubs.Add(03, "Mallorca");
+            clubs.Add(04, "İnter");
+            clubs.Add(05, "Milan");
+            clubs.Add(06, "Juventus");
+            clubs.Add(07, "Atletico Madrid");
+            clubs.Add(08, "Roma");
+
+            foreach (var club in clubs.Values)
+            {
+                Console.WriteLine(club);
+            }
+            foreach (var numbers in clubs.Keys)
+            {
+                Console.WriteLine(numbers);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
